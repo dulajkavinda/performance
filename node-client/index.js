97ef29a13726a5837a25bc410e558c94a3fa0410
dulajkavinda <dulajkaavinda@gmail.com>
2021-09-20 00:17:09 +0530

    os data retrive

--- node-client/index.js
+++ node-client/index.js
@@ -13,15 +13,31 @@ const os = require("os");
 
 const cpus = os.cpus();
 
-const osType = os.type() === "Darwin" ? "Mac" : os.type();
-const uptime = os.uptime();
-const freeMem = os.freemem();
-const totalMem = os.totalmem();
-const usedMem = totalMem - freeMem;
-const memUsage = Math.floor((usedMem / totalMem) * 100) / 100;
-const cpuModel = cpus[0].model;
-const cpuSpeed = cpus[0].speed;
-const numCpus = cpus.length;
+const performanceData = () => {
+  return new Promise(async (resolve, reject) => {
+    const osType = os.type() === "Darwin" ? "Mac" : os.type();
+    const uptime = os.uptime();
+    const freeMem = os.freemem();
+    const totalMem = os.totalmem();
+    const usedMem = totalMem - freeMem;
+    const memUsage = Math.floor((usedMem / totalMem) * 100) / 100;
+    const cpuModel = cpus[0].model;
+    const cpuSpeed = cpus[0].speed;
+    const numCpus = cpus.length;
+    const cpuLoad = await getCpuLoad();
+    resolve({
+      freeMem,
+      totalMem,
+      memUsage,
+      osType,
+      uptime,
+      cpuModel,
+      cpuSpeed,
+      numCpus,
+      cpuLoad,
+    });
+  });
+};
 
 const cpuAverage = () => {
   const cpus = os.cpus();
@@ -43,17 +59,19 @@ const cpuAverage = () => {
 };
 
 const getCpuLoad = () => {
-  const start = cpuAverage();
-  setTimeout(() => {
-    const end = cpuAverage();
-    const idleDifference = end.idle - start.idle;
-    const totalDifference = end.total - start.total;
-    const percentageCpu =
-      100 - Math.floor((100 * idleDifference) / totalDifference);
-    console.log(percentageCpu);
-  }, 100);
+  return new Promise((resolve, reject) => {
+    const start = cpuAverage();
+    setTimeout(() => {
+      const end = cpuAverage();
+      const idleDifference = end.idle - start.idle;
+      const totalDifference = end.total - start.total;
+      const percentageCpu =
+        100 - Math.floor((100 * idleDifference) / totalDifference);
+      resolve(percentageCpu);
+    }, 100);
+  });
 };
 
-setInterval(() => {
-  getCpuLoad();
-}, 1000);
+performanceData().then((allPerformanceData) => {
+  console.log(allPerformanceData);
+});
