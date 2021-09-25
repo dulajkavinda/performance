bf57e4fc6da1f3bfcb7196371c09b49b3a12424e
dulajkavinda <dulajkaavinda@gmail.com>
2021-09-25 17:38:38 +0530

    frontend done

--- node-client/index.js
+++ node-client/index.js
@@ -17,6 +17,7 @@ socket.on("connect", () => {
   const netT = os.networkInterfaces();
   let macA;
   for (let key in netT) {
+    macA = Math.floor(Math.random() * 3) + 1;
     if (!netT[key][0].internal) {
       macA = netT[key][0].mac;
       break;
